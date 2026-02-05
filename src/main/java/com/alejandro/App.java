274e91e27df94c66ade1e6f13e501acc48a0c3a1
Test User <test@example.com>
2026-02-05 18:08:52 -0500

    Initial commit

---
+++ src/main/java/com/alejandro/App.java
@@ -0,0 +1,10 @@
+package com.alejandro;
+
+/**
+ * Order Management System Application
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Order Management System");
+    }
+}
